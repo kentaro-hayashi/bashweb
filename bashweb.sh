6f2ac9ea62d95a677063c89b7ecdd38748ed7bdd
kentaro-hayashi <k@8-3.cc>
2020-07-05 13:24:02 +0900

    feat: use tcpserver instead of netcat

--- bashweb.sh
+++ bashweb.sh
@@ -2,6 +2,7 @@
 
 trap exit INT;
 SCRIPTDIR=$(cd $(dirname $BASH_SOURCE); pwd)
+TCPSERVER=${SCRIPTDIR}/vendor/tcpserver/ucspi-tcp-0.88/tcpserver
 
 case $1 in
 
@@ -12,9 +13,18 @@ case $1 in
     echo "Install dependencies..."
     mkdir -p ${SCRIPTDIR}/vendor/mo
     pushd ${SCRIPTDIR}/vendor/mo
-
     if [ ! -e "./mo" ]; then
       wget https://raw.githubusercontent.com/tests-always-included/mo/master/mo
+    fi
+    popd
+    mkdir -p ${SCRIPTDIR}/vendor/tcpserver
+    pushd ${SCRIPTDIR}/vendor/tcpserver
+    if [ ! -e "${TCPSERVER}" ]; then
+      wget http://cr.yp.to/ucspi-tcp/ucspi-tcp-0.88.tar.gz
+      tar zxvf ucspi-tcp-0.88.tar.gz
+      pushd ucspi-tcp-0.88
+      make
+      popd
     fi
     popd
     mkdir -p ${SCRIPTDIR}/log
@@ -35,7 +45,7 @@ EOF
 
     while :
     do
-      netcat -lp 8080 -e "/bin/bash ${SCRIPTDIR}/app.sh"
+      ${TCPSERVER} 0.0.0.0 8080 /bin/bash ${SCRIPTDIR}/app.sh
     done
     ;;
 
